c6096fa1002f9af4cf34bd71e7dcc96ae368bf93
tonmoy426 <tonmoytanvir426@gmail.com>
2025-12-30 23:48:20 +0600

    Creating a Custom Router

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,20 @@ import config from './config';
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running...")
+    //root route
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/json' });
-        res.end(JSON.stringify({ message: 'Hello, World!',
+        res.end(JSON.stringify({
+            message: 'Hello, World!',
+            path: req.url,
+        })
+     );
+    }
+    //about route
+    if (req.url === '/about' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/json' });
+        res.end(JSON.stringify({
+            message: 'This is the about page',
             path: req.url,
         }));
     }
@@ -16,6 +27,7 @@ const server: Server = http.createServer((req: IncomingMessage, res: ServerRespo
 
 
 
+
 server.listen(config.port, () => {
     console.log(`Server is listening on port ${config.port}`);
-});
+}); 
